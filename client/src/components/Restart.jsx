fedd8cb5ba7c9b1b732db2a25e0ea784821a5d9e
Jonathan <jonathanafugwobi1@gmail.com>
2025-05-22 16:20:34 +0100

    Add Restart Component

---
+++ client/src/components/Restart.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Restart(props) {
+
+    const [isMoused, setIsMoused] = useState(false);
+
+    function mousedOver() {
+        setIsMoused(true);
+    }
+
+    function mousedOut() {
+        setIsMoused(false);
+    }
+
+
+    return (
+        <div className="restart-con">
+            <button 
+            onClick={props.click}
+            style={{
+                backgroundColor: isMoused ? "darkred" : "red",
+                color: "white",
+                cursor: isMoused && "pointer"
+            }}
+            onMouseOver={mousedOver}
+            onMouseOut={mousedOut}
+            >Restart Quiz
+            </button>
+        </div>
+    )
+}
+
+export default Restart;
