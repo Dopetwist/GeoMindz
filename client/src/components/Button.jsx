c5d5b77a3f02b90ec58030589734e991350c15c6
Jonathan <jonathanafugwobi1@gmail.com>
2025-10-06 13:09:45 +0100

    Remove unnecessary imports

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Zoom from '@mui/material/Zoom';
 
 function Button(props) {
--- client/src/components/Country.jsx
+++ client/src/components/Country.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 function Country(props) {
 
