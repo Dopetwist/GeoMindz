5bb1dbba496a266ce46c4514d76a509ebf77beae
Jonathan <jonathanafugwobi1@gmail.com>
2025-10-06 13:55:42 +0100

    feat: create Timer component and work on timer functionality

---
+++ client/src/components/Timer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function Timer() {
+
+    // const now = new Date().toLocaleTimeString();
+
+    const [ count, setCount ] = useState(20);
+
+    useEffect(() => {
+        const decrease = () => {
+            setCount(count - 1);
+        }
+
+        decrease();
+    }, [])
+
+    // function decrease() {
+    //     // const currentTime = new Date().toLocaleTimeString();
+    //     setCount(count - 1);
+    // }
+
+    setInterval(decrease, 1000);
+
+    return (
+        <div>
+            <h1> {count} </h1>
+        </div>
+    )
+}
+
+export default Timer;
