c6c58044911f0b7e09a1809ac666c6ac8b65b37d
Jonathan <jonathanafugwobi1@gmail.com>
2026-03-13 20:38:13 +0100

    fix: change postgres connection to render for deployment

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import env from "dotenv";
-import pg from "pg";
-/* import pkg from "pg"; */
+/* import pg from "pg"; */
+import pkg from "pg";
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-/* const { Pool } = pkg; */
+const { Pool } = pkg;
 
 app.use(cors());
 app.use(express.json());
@@ -16,14 +16,14 @@ app.use(express.json());
 env.config();
 
 // Database Connection
-/* const db = new Pool({
+const db = new Pool({
   connectionString: process.env.DATABASE_URL || "postgres://postgres:J20161978583A...@localhost:5432/paperback",
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false
-}); */
+});
 
-const db = new pg.Client({
+/* const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -31,13 +31,13 @@ const db = new pg.Client({
   port: process.env.DB_PORT
 });
 
-db.connect();
+db.connect(); */
 
 
 
 app.get("/api/countries", async (req, res) => {
   try {
-    const result = await db.query("SELECT * FROM capitals");
+    const result = await db.query("SELECT country, capital FROM public.capitals ORDER BY id ASC");
     res.json(result.rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
